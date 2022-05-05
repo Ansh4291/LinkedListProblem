bc3ca0fdf18ab2c895739766e144bcbeb90c88c3
AnshulRathore <anshulrathore1995@gmail.com>
2022-05-05 10:21:32 +0530

    Added: Create simple linked list 56,30,70

--- src/com/bridgelabz/LinkedListProblem.java
+++ src/com/bridgelabz/LinkedListProblem.java
@@ -1,7 +1,25 @@
 package com.bridgelabz;
+class Node{
+    public int data;
+    public Node address;
 
+    public Node(int data){
+        this.data = data;
+        this.address = null;
+    }
+}
 public class LinkedListProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to the Linked List Programs :-) ");
+            Node firstNode = new Node(56);
+            Node secondNode = new Node(30);
+            Node thirdNode = new Node(70);
+            firstNode.address = secondNode;
+            secondNode.address = thirdNode;
+            Node temp = firstNode;
+            while (temp != null){
+                System.out.println(temp.data);
+                temp = temp.address;
+            }
     }
 }
