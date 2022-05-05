2ccb2cb2c12903aae5b6fbeeabe8dae3e54fd93c
AnshulRathore <anshulrathore1995@gmail.com>
2022-05-06 00:10:52 +0530

    Added: Adding the data

--- src/com/bridgelabz/LinkedListProblem.java
+++ src/com/bridgelabz/LinkedListProblem.java
@@ -1,25 +1,17 @@
 package com.bridgelabz;
-class Node{
-    public int data;
-    public Node address;
 
-    public Node(int data){
-        this.data = data;
-        this.address = null;
-    }
-}
+
 public class LinkedListProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to the Linked List Programs :-) ");
-            Node firstNode = new Node(56);
-            Node secondNode = new Node(30);
-            Node thirdNode = new Node(70);
-            firstNode.address = secondNode;
-            secondNode.address = thirdNode;
-            Node temp = firstNode;
-            while (temp != null){
-                System.out.println(temp.data);
-                temp = temp.address;
-            }
+        //taking object of operation class
+        //and passing the data
+        Operations operations = new Operations();
+        operations.add(70);
+        operations.add(30);
+        operations.add(56);
+        //Calling the print method inside main method
+        operations.print();
     }
+
 }
