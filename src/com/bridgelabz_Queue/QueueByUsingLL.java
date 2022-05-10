8bbb0104a4bf1f4709881e9c687ce40e0d72ec39
AnshulRathore <anshulrathore1995@gmail.com>
2022-05-10 13:16:51 +0530

    Added:Dequeue is added

--- src/com/bridgelabz_Queue/QueueByUsingLL.java
+++ src/com/bridgelabz_Queue/QueueByUsingLL.java
@@ -3,10 +3,15 @@ package com.bridgelabz_Queue;
 public class QueueByUsingLL {
     public static void main(String[] args) {
        Operation operation = new Operation();
-
+        //Adding into Queue
         operation.enqueue(56);
         operation.enqueue(30);
         operation.enqueue(70);
+        System.out.println("Queue Front : " + operation.front.data);
+        System.out.println("Queue Rear : " + operation.rear.data);
+        //Deleting The Data From Queue
+        operation.dequeue();
+        System.out.println("After Front And Rear");
         System.out.println("Queue Front : " + operation.front.data);
         System.out.println("Queue Rear : " + operation.rear.data);
     }
